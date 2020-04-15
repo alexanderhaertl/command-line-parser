8c942ea0e0b5cbc9b7dccd25cf77042ba3f20928
Alexander Härtl <allli@gmx.net>
2020-04-15 10:06:33 +0200

    changed parsing: - the named options implicitly start with '-' - there are optional and mandatory unnamed parameters

--- src/CommandLineOptionParser.hpp
+++ src/CommandLineOptionParser.hpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <map>
 #include <functional>
+#include <algorithm>
 
 namespace StringParsing
 {
@@ -29,50 +30,105 @@ public:
     m_parserFunctions.emplace(optionName, std::bind(StringParsing::parseString<T>, std::placeholders::_1, std::ref(parameter)));
   }
 
-  template <>
-  void registerOption(bool& flag, const std::string& optionName)
+  void registerSwitch(bool& parameter, const std::string& parameterName)
   {
-    m_flags.emplace(optionName, &flag);
+    m_flags.emplace(parameterName, &parameter);
   }
 
-  bool parseCommandLineArguments(const std::vector<std::string>& args)
+  template <typename T>
+  void registerUnnamedParameter(T& parameter, const std::string& parameterName, bool mandatory = true)
   {
-    for (auto i = args.begin(); i != args.end(); ++i)
+    m_unnamedParameters.push_back({
+      std::bind(StringParsing::parseString<T>, std::placeholders::_1, std::ref(parameter)),
+      parameterName, 
+      mandatory });
+  }
+
+  bool parseCommandLineArguments(const std::vector<std::string>& args) const
+  {
+    std::vector<const UnnamedParameter*> mandatoryParams, optionalParams;
+    for (const auto& i : m_unnamedParameters)
     {
-      auto flagFindResult = m_flags.find(*i);
-      auto paramFindResult = m_parserFunctions.find(*i);
-      if (flagFindResult != m_flags.end())
+      (i.mandatory ? mandatoryParams : optionalParams).push_back(&i);
+    }
+    auto mandatoryParamIterator = mandatoryParams.begin();
+    auto optionalParamIterator = optionalParams.begin();
+
+    for (auto i = ++args.begin(); i != args.end(); ++i)
+    {
+      if (i->substr(0, 1) == "-")
       {
-        *(flagFindResult->second) = !(*(flagFindResult->second));
-      }
-      else if (paramFindResult != m_parserFunctions.end())
-      {
-        if (++i != args.end()) // we have at least one parameter left
+        // named option
+        const auto optionIdentifier = i->substr(1);
+        auto flagFindResult = m_flags.find(optionIdentifier);
+        auto paramFindResult = m_parserFunctions.find(optionIdentifier);
+        if (flagFindResult != m_flags.end())
         {
-          // parameter iterator was intentionally advanced, because the actual option is the parameter following the option identifier
-          if (!paramFindResult->second(*i))
+          *(flagFindResult->second) = !(*(flagFindResult->second));
+        }
+        else if (paramFindResult != m_parserFunctions.end())
+        {
+          if (++i != args.end()) // we have at least one parameter left
           {
-            // parsing error
+            // parameter iterator was intentionally advanced, because the actual option is the parameter following the option identifier
+            if (!paramFindResult->second(*i))
+            {
+              // parsing error
+              return false;
+            }
+          }
+          else
+          {
+            // we have an option expecting a parameter, without a parameter following
             return false;
           }
         }
         else
         {
-          // we have an option expecting a parameter, without a parameter following
+          // unknown option
           return false;
         }
       }
       else
       {
-        // unknown option
-        return false;
+        // unnamed option
+        if (mandatoryParamIterator != mandatoryParams.end())
+        {
+          // we have mandatory parameter to match
+          if (!(**mandatoryParamIterator++).parserFunction(*i)) return false;
+        }
+        else if (optionalParamIterator != optionalParams.end())
+        {
+          // we have an optional parameter to match
+          if (!(**optionalParamIterator++).parserFunction(*i)) return false;
+        }
+        else
+        {
+          // we have an unnamed parameter without registered counterpart
+          return false;
+        }
       }
+    }
+
+    // check that all mandatory parameters are matched
+    if (mandatoryParamIterator != mandatoryParams.end())
+    {
+      // there are remaining mandatory parameters without matching command line argument
+      return false;
     }
 
     return true;
   }
 
 private:
+  struct UnnamedParameter
+  {
+    std::function<bool(const std::string&)> parserFunction;
+    std::string name;
+    bool mandatory;
+  };
+
+  std::vector<UnnamedParameter> m_unnamedParameters;
   std::map<std::string, std::function<bool(const std::string&)> > m_parserFunctions;
   std::map<std::string, bool*> m_flags;
 };
